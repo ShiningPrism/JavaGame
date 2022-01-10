ec04a8990611c0a46d4d2e64c45e0c22502ca7b2
Roman <61665029+ShiningPrism@users.noreply.github.com>
2022-01-10 22:25:44 +0200

    Update Main.java
    Deleted some unnecessary information.

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,6 @@
     |          Play and have fun!          |
     |         VK/TG: @ShiningPrism         |
     \-------------------------------------*/
-    /*
-        2.2.1.3
-     • The system of prompts that you can go to the next level has been reworked;
-     • The system for removing points for meaningless text has been deleted;
-     */
     public class Main {
         public static void main(String[] args) {
             Game startGame = new Game();
